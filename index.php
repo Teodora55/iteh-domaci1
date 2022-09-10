5bec8353fbffb419b7aa9c0fc7ad96f4195b2d9c
Teodora55 <beljicteodora55@gmail.com>
2022-09-10 18:50:39 +0200

    Added login form

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>   
+<html>   
+<head>  
+<meta name="viewport" content="width=device-width, initial-scale=1">  
+<title> Login Page </title> 
+<link rel="stylesheet" href="css\styles.css">   
+</head>    
+<body>    
+<div class="container">
+    <p class="heading">Login In</p>
+    <div class="box">
+        <p>Email</p>
+        <div> <input type="text" autocomplete="off" placeholder="Enter your email"> </div>
+    </div>
+    <div class="box">
+        <p>Email</p>
+        <div> <input type="text" autocomplete="off" placeholder="Enter your password"> </div>
+    </div> <button class="loginBtn">Login</button>
+    <p class="text">Don't have an account? <a href="#">Signup</a></p>
+</div>
+</body>     
+</html> 
